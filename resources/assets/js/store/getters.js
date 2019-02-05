db3315d6a95b1b60637e5bece89c80fcfac9d58c
upathissa <amilaupathissa@gmail.com>
2019-02-05 11:17:27 +0530

    master

---
+++ resources/assets/js/store/getters.js
@@ -0,0 +1,3 @@
+export const getNumber = (state) => {
+    return state.number;
+}
---
+++ resources/assets/js/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+import state from './state'
+import * as actions from './actions'
+import * as getters from './getters'
+import * as mutations from './mutations'
+
+export default new Vuex.Store({
+    state,
+    actions,
+    getters,
+    mutations
+})
